2101e8394a9591ee5b9189dfa4eb34596c270b43
Viktor Dimitrov <viktor.dimitrov.1989@gmail.com>
2017-01-23 22:24:26 +0200

    First commmit BashSoft

---
+++ src/OutputWriter.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+public class OutputWriter{
+    public static void writeMessage(String message){
+        System.out.print(message);
+    }
+
+    public static void writeMessageOnNewLine(String message){
+        System.out.println(message);
+    }
+
+    public static void writeEmptyLine(){
+        System.out.println();
+    }
+
+    public static void displayException(String message){
+        System.out.println(message);
+    }
+
+    public static void traverseDirectory(String path){
+        LinkedList<File> subFolders = new LinkedList<File>();
+        File root = new File(path);
+
+        subFolders.add(root);
+
+        while (!subFolders.isEmpty()){
+            File currentFolder = subFolders.removeFirst();
+
+            if(currentFolder.listFiles() != null){
+                for (File file: currentFolder.listFiles()){
+                    if(file.isDirectory()){
+                        for (File fileInDir: file.listFiles()){
+                            subFolders.add(fileInDir);
+                        }
+                    }
+                }
+            }
+            System.out.println(currentFolder.toString());
+        }
+    }
+
+    public static void printStudent(String name, List<Integer> marks){
+        String output = String.format("%s - %s", name, marks.toString());
+        OutputWriter.writeMessageOnNewLine(output);
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,11 @@
+public class Program {
+    public static void main(String[] args) {
+        //String path1 = "E:\\Softuni\\Softuni Courses - Data\\Java Courses\\Java Advanced - old\\3. Files and Directories\\test1.txt";
+        //String path2 = "E:\\Softuni\\Softuni Courses - Data\\Java Courses\\Java Advanced - old\\3. Files and Directories\\test2.txt";
+
+        //Tester.compareContent(path1,path2);
+
+        IOManager.createDirectoryInCurrentFolder("pesho");
+
+    }
+}
